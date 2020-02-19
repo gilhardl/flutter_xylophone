3a758ab98eaca4d3eef21a96f63f52a822003280
Lucas GILHARD <l.gilhard@gmail.com>
2020-02-19 12:15:18 +0100

    implements xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -9,8 +9,44 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Bar(soundNumber: 1, color: Colors.red),
+              Bar(soundNumber: 2, color: Colors.orange),
+              Bar(soundNumber: 3, color: Colors.yellow),
+              Bar(soundNumber: 4, color: Colors.green),
+              Bar(soundNumber: 5, color: Colors.teal),
+              Bar(soundNumber: 6, color: Colors.blue),
+              Bar(soundNumber: 7, color: Colors.deepPurple),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class Bar extends StatelessWidget {
+  Bar({this.soundNumber, this.color});
+
+  final int soundNumber;
+  final Color color;
+
+  void playSound() {
+    final AudioCache player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound();
+        },
+        color: color,
+        child: null,
       ),
     );
   }
